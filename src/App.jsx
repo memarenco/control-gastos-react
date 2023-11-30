ad51c1544e41f7e1e2543801a785265f949e9544
Melvin Marenco <memarenco@gmail.com>
2023-11-30 11:10:21 -0600

    Mostrando componente de Gastos si Presupuesto es válido

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import Header from "./components/Header";
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(0);
   return (
     <div>
-      <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto} />
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
     </div>
   );
 }
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import Mensaje from "./Mensaje";
-const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+const NuevoPresupuesto = ({
+  presupuesto,
+  setPresupuesto,
+  setIsValidPresupuesto,
+}) => {
   const [mensaje, setMensaje] = useState(0);
   const handlePresupuesto = (e) => {
     e.preventDefault();
-    if (Number(presupuesto) <= 0) {
+    if (presupuesto <= 0) {
       setMensaje("No es un presupuesto válido");
-    } else {
-      setMensaje("Sí es valido");
+      return;
     }
+    setMensaje("");
+    setIsValidPresupuesto(true);
   };
   return (
     <div className="contenedor-presupuesto contenedor sombra">
@@ -20,7 +25,7 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
             className="nuevo-presupuesto"
             placeholder="Añade tu Presupuesto"
             defaultValue={presupuesto}
-            onChange={(e) => setPresupuesto(e.target.value)}
+            onChange={(e) => setPresupuesto(Number(e.target.value))}
           />
         </div>
         <input type="submit" value="Añadir" />
