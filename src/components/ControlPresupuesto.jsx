7b13eedca32a46b281e4f7f39d0432026e34b0a8
Melvin Marenco <memarenco@gmail.com>
2023-11-30 11:31:09 -0600

    Convirtiendo números en formato moneda

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 const ControlPresupuesto = ({ presupuesto }) => {
+  const formatearCantidad = (cantidad) => {
+    return presupuesto.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
@@ -8,7 +14,13 @@ const ControlPresupuesto = ({ presupuesto }) => {
       </div>
       <div className="contenido-presupuesto">
         <p>
-          <span>Presupuesto:</span> {presupuesto}
+          <span>Presupuesto:</span> {formatearCantidad(presupuesto)}
+        </p>
+        <p>
+          <span>Disponible:</span> {formatearCantidad(0)}
+        </p>
+        <p>
+          <span>Gastado:</span> {formatearCantidad(0)}
         </p>
       </div>
     </div>
