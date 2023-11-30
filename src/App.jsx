4eb1522a3c00cf98c4dbe97ccd0c32a647b32e31
Melvin Marenco <memarenco@gmail.com>
2023-11-30 12:08:30 -0600

    Añadiendo botón para registrar nuevos gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(0);
@@ -11,6 +12,16 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img
+            src={IconoNuevoGasto}
+            alt="Icono Nuevo Gasto"
+            className="img-fluid"
+          />
+        </div>
+      )}
     </div>
   );
 }
